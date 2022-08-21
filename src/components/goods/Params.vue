58ee6487dfaca760e3af9e8412a38f4454261d27
xiaoyegecode <1010039036@qq.com>
2022-08-21 15:28:21 +0800

    完成了分类参数开发

---
+++ src/components/goods/Params.vue
@@ -0,0 +1,143 @@
+<template>
+  <div>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>分类参数</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <!-- 警告区域 :closable="false"(是否展示“X”号) show-icon(显示图标) -->
+      <el-alert title="注意：只允许为第三级分类设置相关参数" type="warning" :closable="false" show-icon>
+      </el-alert>
+
+      <!-- 选择商品分类区域 -->
+      <el-row class="cat_opt">
+        <el-col>
+          <span>选择商品分类：</span>
+          <!-- 选择商品分类的级联选择框 -->
+          <el-cascader expandTrigger='hover' v-model="selectedCateKeys" :options="cateList" :props="cateProps"
+            @change=" handleChange">
+          </el-cascader>
+        </el-col>
+      </el-row>
+
+      <!-- tab页签区 -->
+      <el-tabs v-model="activeName" @tab-click="handleTabClick">
+        <!-- 添加动态参数的面板 将标签页改为many -->
+        <el-tab-pane label="动态参数" name="many">
+          <el-button size="mini" type="primary" :disabled="isButtonDisabled">添加参数</el-button>
+          <!-- 动态参数表格 -->
+          <el-table :data="manyTableData" border stripe>
+            <!-- 展开行 -->
+            <el-table-column type="expand"></el-table-column>
+            <!-- 索引列 -->
+            <el-table-column type="index"></el-table-column>
+            <el-table-column label="参数名称" prop="attr_name"></el-table-column>
+            <el-table-column label="操作">
+              <template slot-scope="scope">
+                <el-button size="mini" type="primary" icon="el-icon-edit">编辑</el-button>
+                <el-button size="mini" type="danger" icon="el-icon-delete">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-tab-pane>
+        <!-- 添加静态属性的面板 将标签页改为only -->
+        <el-tab-pane label="静态属性" name="only">
+          <el-button size="mini" type="primary" :disabled="isButtonDisabled">添加属性</el-button>
+          <!-- 静态属性表格 -->
+          <el-table :data="onlyTableData" border stripe>
+            <!-- 展开行 -->
+            <el-table-column type="expand"></el-table-column>
+            <!-- 索引列 -->
+            <el-table-column type="index"></el-table-column>
+            <el-table-column label="属性名称" prop="attr_name"></el-table-column>
+            <el-table-column label="操作">
+              <template slot-scope="scope">
+                <el-button size="mini" type="primary" icon="el-icon-edit">编辑</el-button>
+                <el-button size="mini" type="danger" icon="el-icon-delete">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-tab-pane>
+
+      </el-tabs>
+    </el-card>
+
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cateList: [],
+      selectedCateKeys: [],
+      cateProps: {
+        value: 'cat_id',
+        label: 'cat_name',
+        children: 'children'
+      },
+      //tab页签激活显示的页签项
+      activeName: 'many',
+      //用来保存动态参数数据
+      manyTableData: [],
+      //用来保存静态属性数据
+      onlyTableData: []  
+    }
+  },
+  created() {
+    this.getCateList()
+  },
+  methods: {
+    async getCateList() {
+      const { data: res } = await this.$http.get('categories')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.cateList = res.data
+    },
+    handleChange() {
+      this.getParamsData()
+    },
+    handleTabClick() {
+      this.getParamsData()
+    },
+    //获取参数列表数据
+    async getParamsData() {
+      if (this.selectedCateKeys.length !== 3) {
+        this.selectedCateKeys = []
+        return
+      }
+      const { data: res } = await this.$http.get(`categories/${this.cateId}/attributes`,
+        { params: { sel: this.activeName } })
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      if (this.activeName === 'many') {
+        this.manyTableData = res.data
+      } else if (this.activeName === 'only') {
+        this.onlyTableData = res.data
+      }
+    }
+
+  },
+  computed: {
+    isButtonDisabled() {
+      if (this.selectedCateKeys.length !== 3) {
+        return true
+      }
+      return false
+    },
+    cateId() {
+      if (this.selectedCateKeys.length === 3) {
+        return this.selectedCateKeys[this.selectedCateKeys.length - 1]
+      }
+      return null
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -30,7 +30,9 @@ import {
   Select,
   Option,
   Cascader,
-
+  Alert,
+  Tabs,
+  TabPane
 } from 'element-ui'
 
 Vue.use(Button)
@@ -61,6 +63,9 @@ Vue.use(Tree)
 Vue.use(Select)
 Vue.use(Option)
 Vue.use(Cascader)
+Vue.use(Alert)
+Vue.use(Tabs)
+Vue.use(TabPane)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
 
